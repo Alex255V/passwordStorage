009aafdc59d96131ab4eb960183953c4c027136d
Alexander <Visafer.lord@yandex.ru>
2017-01-20 22:12:30 +0200

    myCommit

--- PasswordStorage.java
+++ PasswordStorage.java
@@ -6,6 +6,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Arrays;
 
+import static java.awt.Color.BLUE;
+
 /**
  * Created by Alex on 12.01.2017.
  * Create to save passwords
@@ -112,14 +114,116 @@ public class PasswordStorage extends JPanel implements ActionListener {
 
 //---------------------------next window----------->menuPro-------------------------------------------------\\
 
-class menuPro extends JFrame  {
+class menuPro extends JFrame implements ActionListener {
 
     static menuPro mp = new menuPro();
+    private static menuAddClass mdc = new menuAddClass();
 
     private static JButton addRecord;
     private static JButton rmRecord;
     private static JButton search;
     private static JTextField enterSearch;
 
+//    private JFrame menuAdd = new JFrame();   ---------> work with BD
+
+    private menuPro() {
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("W E L C O M E");
+        setSize(500,300);
+
+        setLayout (new FlowLayout (FlowLayout.CENTER, 10, 10));
+
+        addRecord = new JButton("add record");
+        addRecord.setBackground(new Color(0,0,255));
+        addRecord.setSize(90,20);
+        add(addRecord);
+        addRecord.addActionListener(this);
+
+        rmRecord = new JButton("remove record");
+        rmRecord.setBackground(new Color(0,0,255));
+        rmRecord.setSize(90,20);
+        add(rmRecord);
+        rmRecord.addActionListener(this);
+
+        enterSearch = new JTextField(25);
+        enterSearch.setLocation(25,60);
+//        enterSearch.getText();
+        add(enterSearch);
+
+        search = new JButton("search");
+        search.setBackground(new Color(0,0,0));
+        search.setLocation(60,90);
+        add(search);
+        search.addActionListener(this);
+
+        getContentPane().setBackground(new Color(27, 39, 39));
+        setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == search) {
+
+        }
+
+        if (ae.getSource() == addRecord) {
+            mdc.setVisible(true);
+        }
+
+//         menuAdd ;
+//        {
+//            setSize(300,150);
+//            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//            setLocationRelativeTo(null);
+//            getContentPane().setBackground(new Color(25,45,40));
+//            setVisible(true);
+//
+//        }
+    }
+}
+//-------------------------------------------------------------------------------\\
+class menuAddClass extends JFrame {
+//    public static menuAddClass mdc = new menuAddClass();
+
+    private static JButton OK;
+
+    menuAddClass() {
+        super("create new data");
+        setSize(350,135);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        getContentPane().setBackground(new Color (0,0,0));
+
+        setLayout (null);
+
+        JLabel logLab = new JLabel();
+        logLab.setForeground(new Color(100,149,237));
+        logLab.setText("data name:");
+        logLab.setBounds(5,10,80,20);
+        add(logLab);
+
+        JLabel passLab = new JLabel();
+        passLab.setForeground(new Color(210,105,30));
+        passLab.setText("password:");
+        passLab.setBounds(8,40,80,20);
+        add(passLab);
+
+
+        JTextField name = new JTextField(20);
+        name.setBackground(Color.BLACK);
+        name.setBounds(85,10,250,20);
+        add(name);
+
+        JTextField pass = new JTextField(20);
+        pass.setBackground(Color.BLACK);
+        pass.setBounds(85,40,250,20);
+        add(pass);
+
+        OK = new JButton("OK");
+        OK.setBackground(BLUE);
+        OK.setBounds(350/2-25,70,55,25);
+        add(OK);
+    }
 
 }
